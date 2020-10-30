4bc1b7a462728bae140bbc962b2f829889e74676
whitedevil31 <magz3116@gmail.com>
2020-10-30 14:20:40 +0530

    faq added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import "./App.css";
 import React from "react";
-import { Helmet } from "react-helmet";
-import { Card } from "@material-ui/core";
+import questions from "./data";
+import Component from "./Component";
 
 function App() {
   return (
     <div className="body">
-      <div className="card-component">
-        <p className="text">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-      </div>
+      <h2>Questions and Answers about login</h2>
+      {questions.map((question) => {
+        return <Component key={question.id} {...question}></Component>;
+      })}
     </div>
   );
 }
--- src/component.js
+++ src/component.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-function component() {
-  return <div></div>;
-}
+import AddIcon from "@material-ui/icons/Add";
+import "./App.css";
 
-export default component;
+const Component = ({ title, info }) => {
+  const [showinfo, setInfo] = useState(false);
+  return (
+    <div className="container">
+      <div className="section">
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setInfo(!showinfo)}>
+          <AddIcon />
+        </button>
+      </div>
+      {showinfo && <p>{info}</p>}
+    </div>
+  );
+};
+
+export default Component;
