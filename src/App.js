464ba73469956bc557251e577587e9c9790d731c
whitedevil31 <magz3116@gmail.com>
2020-11-14 18:42:15 +0530

    tailwind css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
 import React from "react";
 import questions from "./data";
 import Component from "./Component";
 
 function App() {
   return (
-    <div className="body">
+    <div className="container">
       <h2>Questions and Answers about login</h2>
-      {questions.map((question) => {
+      <button> CLICK</button>
+      {/* {questions.map((question) => {
         return <Component key={question.id} {...question}></Component>;
-      })}
+      })} */}
     </div>
   );
 }
