a1b8c7a0e108686466817ae89aadd82e9b70d28d
whitedevil31 <magz3116@gmail.com>
2021-02-18 01:49:58 +0530

    css check

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,10 @@
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
 module.exports = {
   plugins: [
-    tailwindcss("./tailwind.js"),
-    autoprefixer,
+    require("tailwindcss"),
+    require("autoprefixer"),
     require("@fullhuman/postcss-purgecss")({
       content: ["./src/*.js", "./public/index.html"],
+      css: ["./src/assets/tailwind.css"],
       defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
     }),
   ],
